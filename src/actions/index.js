b885e8aab892f731f6cca722304fb28daae5db32
mablanco <mblanco@marketgoo.com>
2020-07-17 17:25:12 +0200

    add renderlist method

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () =>  async dispatch => {
-        const response = await jsonPlaceholder.get('/post');
-        dispatch({ type: 'FETCH_POST', payload: response.data })
+        const response = await jsonPlaceholder.get('/posts');
+        dispatch({ type: 'FETCH_POSTS', payload: response.data })
 };
 
