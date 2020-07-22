cb8835257d37fd86aed2d76ecacde603980b920f
mablanco <mblanco@marketgoo.com>
2020-07-22 09:21:43 +0200

    add PostsandUsers

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions';
+import { fecthPostsAndUsers } from '../actions';
+import UserHeader from './UserHeader';
 
 
 class PostList extends React.Component {
 
     componentDidMount(){
-        this.props.fetchPosts();
+        this.props.fecthPostsAndUsers();
+
     }
 
     renderList() {
@@ -19,6 +21,7 @@ class PostList extends React.Component {
                             <h2>{post.title}</h2>
                             <p>{post.body}</p>
                         </div>
+                        <UserHeader userId={post.userId}/>
                     </div>
                 </div>
             );
@@ -36,5 +39,5 @@ const mapStateToProps = (state) => {
 }
 export default connect(
     mapStateToProps,
-    { fetchPosts }
+    { fecthPostsAndUsers }
     )(PostList);
